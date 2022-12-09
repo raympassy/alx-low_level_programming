90542d760e7af84f7a4e00e8fff8978532a29fd7
raympassy <moutopascale@gmail.com>
2022-12-08 18:31:56 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
  * main - entry
  * Return : Always 0
- * /
- int main (void)
-
- 	puts("\"Programming is like building a multilingual puzzle");
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
+}
