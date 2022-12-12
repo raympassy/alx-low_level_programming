2c302bee26cf49c133fbcb7c2c3cb7b44baca723
raympassy <moutopascale@gmail.com>
2022-12-12 03:48:08 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-ho:wq
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry
+ * Return: 1 Always
+*/
 
+int main(void)
+{
+	write(1, "and that piece of art is - Dora Korpar, 215-10-19\n", 59);
+	return (1);
+}
