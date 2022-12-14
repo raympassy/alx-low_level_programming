8e3730f05c0ea11169206bf4ed8e58476467dbe6
raympassy <moutopascale@gmail.com>
2022-12-14 15:37:43 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print alphabet - prints the alphabet , in lowercase
+ *
+ * Return: ways 0 (Success)
+*/
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+			c++;
+	}
+
+	_putchar('\n');
+}
