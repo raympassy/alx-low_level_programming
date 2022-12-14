58bedca308af4190c875906353b51bf072d408a3
raympassy <moutopascale@gmail.com>
2022-12-14 14:59:22 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
-#include main.h
-
+#include "main.h"
 
 /**
  * main - prints _putchar and a new line
- * Reeturn: Always 0
+ *Return: Always 0
 */
 int main(void)
-
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,7 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
-#define MAIN_<h
+#define MAIN_H
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jackbauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 int _putchar(char c);
 
