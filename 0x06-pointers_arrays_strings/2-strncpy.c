7e5a9e35c38dd9bd2fc8de4bfa125d2467fe87d8
raympassy <moutopascale@gmail.com>
2022-12-22 14:31:02 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the buffer storing copies
+ * @src: the source string
+ * @n: number of bytes
+ * Return: a pointer to the string resulting
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
