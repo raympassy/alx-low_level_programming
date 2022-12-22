9a37670ac76fa49c06126598819fd3e1b46ca8a1
raympassy <moutopascale@gmail.com>
2022-12-22 15:19:04 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,17 +5,18 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: if str1 < str2, negative difference of the first unmatched char
- *  if str1 == str2, 0
- *  if str1 > str2, the positive difference of the first unmatched char
+ * Return: if s1 < s2, negative int, if str1 == str2, 0,
+ *  if str1 > str2, the positive int
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && s1 == *s2)
+	int x;
+
+	for  (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
 	{
-		s1++;
-		s2++;
+		if (s1[x] != s2[x])
+			return (s1[x] = s2[x]);
 	}
-	return (*s1 = s2);
+	return (0);
 }
