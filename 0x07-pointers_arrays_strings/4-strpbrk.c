c327d5d005cd81c9c91d2e6e44a2074384fced70
raympassy <moutopascale@gmail.com>
2023-01-16 13:29:42 -0800

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
-#include <stdio.h>
-
 /**
- * _strpbrk -  prints the consecutive caracters of s1 that are in s2
+ * _strpbrk - prints the consecutive caracters of s1 that are in s2
  * @s: source string
  * @accept: searching string
  *
