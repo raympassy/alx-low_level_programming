e194dbbb5cf9c38ac07bc648bd8df79eb0959628
raympassy <moutopascale@gmail.com>
2023-01-22 18:24:23 -0800

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,13 +18,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
+
 	{
 		 printf("%d", va_arg(numbers, int));
 
-		if (i != (n - 1) && separator != NULL)
+	if (i < n - 1 && separator != NULL)
 
 			 printf("%s", separator);
 	}
+
 	 printf("\n");
 
 	 va_end(numbers);
